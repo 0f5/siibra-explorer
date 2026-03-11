5775cddffad19ffeadd295494e0a9c1f5b9387e8
Xiao Gui <xgui3783@gmail.com>
2026-03-11 10:19:21 +0100

    feat: vanilla viewer configurability experimental cd: cdn viewer

--- third_party/vanilla_nehuba.js
+++ third_party/vanilla_nehuba.js
@@ -3,5 +3,17 @@
     console.warn(`export_nehuba is not defined. Did you forget to import vanilla nehuba?`)
     return
   }
-  window.nehubaViewer = export_nehuba.createNehubaViewer({}, err => console.error(err))
+  const paramsString = window.location.search;
+  const searchParams = new URLSearchParams(paramsString);
+  const keys = [
+    "zoomWithoutCtrl",
+    "rightClickWithCtrl",
+  ]
+  const config = {}
+  for (const key of keys){
+    if (searchParams.has(key)) {
+      config[key] = true
+    }
+  }
+  window.nehubaViewer = export_nehuba.createNehubaViewer(config, err => console.error(err))
 })()
